a9477553e74f581bfc5abb68fa55df2c56a0782e
o.amisrhir@netopia.ma <o.amisrhir@netopia.ma>
2025-02-28 14:34:05 +0000

    refactoring

--- chapitre01/ch01/ch01-01/02-ProductWeb/src/main/java/com/oualid/productweb/ProductWebApplication.java
+++ chapitre01/ch01/ch01-01/02-ProductWeb/src/main/java/com/oualid/productweb/ProductWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapitre01Application {
+public class ProductWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Chapitre01Application.class, args);
+        SpringApplication.run(ProductWebApplication.class, args);
     }
 
 }
