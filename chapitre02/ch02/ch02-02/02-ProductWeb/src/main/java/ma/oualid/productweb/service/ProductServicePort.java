9cbc8fc459938a2e7ba86cc02f30c65bb2552e35
amisrhir oualid <amisrhiroualid94@gmail.com>
2025-03-02 22:52:29 +0000

    refactoring ProductServicePort.

--- chapitre02/ch02/ch02-02/02-ProductWeb/src/main/java/ma/oualid/productweb/service/ProductServicePort.java
+++ chapitre02/ch02/ch02-02/02-ProductWeb/src/main/java/ma/oualid/productweb/service/ProductServicePort.java
@@ -3,28 +3,25 @@ package ma.oualid.productweb.service;
 import ma.oualid.productweb.model.Product;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 public interface ProductServicePort {
 
-    @RequestMapping(value = "/products", method = RequestMethod.GET ,produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/products",produces = {MediaType.APPLICATION_JSON_VALUE})
     ResponseEntity<List<Product>> getAllProducts();
 
-    @RequestMapping(value = "/products/{productId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/products/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Product> getProduct(@PathVariable("productId") String productId);
 
-    @RequestMapping(value = "/products", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Product> addProduct(@RequestBody Product product);
 
-    @RequestMapping(value = "/products/{productId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/products/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Product> deleteProduct(@PathVariable("productId") String productId);
 
-    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Product> updateProduct(@PathVariable("id") String productId, @RequestBody Product product);
+    @PutMapping(value = "/products/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<Product> updateProduct(@PathVariable("id") String productId, @RequestBody Product product);
 
 }
